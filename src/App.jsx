3331e51a1fc73e98c0bee21ecc9d57fe63b59c1c
BassaniDavid <davbassan98@gmail.com>
2025-04-16 15:45:46 +0200

    feat: all bonuses completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,25 @@ import Posts from './routes/posts'
 import WhoWeAre from './routes/WhoWeAre'
 import Homepage from './routes/Homepage'
 import DefaultLayout from './layouts/DefaultLayout'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 
 function App() {
+  const [posts, setPosts] = useState([])
 
+  function fetchPosts() {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(res => setPosts(res.data))
+  }
+
+  useEffect(fetchPosts, [])
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<DefaultLayout />}>
           <Route index element={<Homepage />} />
-          <Route path="posts" element={<Posts />} />
+          <Route path="posts" element={<Posts posts={posts} />} />
           <Route path="chi-siamo" element={<WhoWeAre />} />
         </Route>
       </Routes>
--- src/routes/posts.jsx
+++ src/routes/posts.jsx
@@ -1,5 +1,16 @@
-export default function Posts() {
+export default function Posts({ posts }) {
     return (
-        <h1>Posts</h1>
+        <>
+            <h1>Posts:</h1>
+            <ul className="container-posts">
+                {posts.map(post => (
+                    <li key={post.id}>
+                        <h3>post numero {post.id}</h3>
+                        <h3 className="title">titolo: {post.title}</h3>
+                        <p>{post.body}</p>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
