0415bedc1bfb3836cff11ed6506682c0dd76aeef
BassaniDavid <davbassan98@gmail.com>
2025-04-16 14:40:31 +0200

    feat: created homepage, posts and WhoWeAre jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Posts from './routes/posts'
+import WhoWeAre from './routes/WhoWeAre'
+import Homepage from './routes/Homepage'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Homepage} />
+        <Route path="/posts" Component={Posts} />
+        <Route path="/chi-siamo" Component={WhoWeAre} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
