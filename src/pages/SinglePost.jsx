c0a2a3697ffba118a4654045792f8cbf2578828a
BassaniDavid <davbassan98@gmail.com>
2025-04-17 16:29:01 +0200

    feat: bonus completed

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom"
 
 // importo axios
@@ -23,7 +23,25 @@ export default function SinglePost() {
             .catch(err => console.log(err))
 
 
-    }, [])
+    }, [{ id }])
+
+    // funzione per andare al post successivo
+    function postPlus() {
+        if (id < 100) {
+            return (parseInt(id) + 1)
+        }
+        return id
+    }
+
+    // funzione per andare al post precedente
+    function postminus() {
+        if (id >= 2) {
+            return (parseInt(id) - 1)
+        }
+        else {
+            return id
+        }
+    }
 
     return (
         <div className="singlepost">
@@ -31,6 +49,12 @@ export default function SinglePost() {
             <h4>dettagli:</h4>
             <p>{postDetails.title}</p>
             <button onClick={() => navigate(-1)}>torna alla pagina preceedente</button>
+            <Link to={`/posts/${postPlus()}`}>
+                <button>prossimo post</button>
+            </Link>
+            <Link to={`/posts/${postminus()}`}>
+                <button>post precedente</button>
+            </Link>
         </div>
     )
 }
