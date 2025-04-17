973352ea7e9c56388a50cb0717aa1853e6feed96
BassaniDavid <davbassan98@gmail.com>
2025-04-17 16:11:35 +0200

    feat: style improved

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
 
 // importo axios
 import axios from 'axios'
 
 export default function SinglePost() {
+
+    const navigate = useNavigate()
     const { id } = useParams()
 
     const [postDetails, setPostsDetails] = useState({})
@@ -23,10 +26,11 @@ export default function SinglePost() {
     }, [])
 
     return (
-        <div>
+        <div className="singlepost">
             <p>numero post: {id}</p>
-            <h3>dettagli</h3>
+            <h4>dettagli:</h4>
             <p>{postDetails.title}</p>
+            <button onClick={() => navigate(-1)}>torna alla pagina preceedente</button>
         </div>
     )
 }
